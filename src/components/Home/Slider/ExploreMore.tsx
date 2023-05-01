52280f5c0c7c4b2f039389544084b7ffe2db4334
Tulio <tulioruzo29@gmail.com>
2023-05-01 17:44:28 -0400

    Add ExploreMore Button to Slider in home

---
+++ src/components/Home/Slider/ExploreMore.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+type Props = {};
+
+export default function ExploreMore({}: Props) {
+   const [stringArray, setStringArray] = useState<string[]>([]);
+   useEffect(() => {
+      const string = "Explore More • Explore More • ";
+      setStringArray(string.split(""));
+   }, []);
+
+   return (
+      <div className="relative rounded-full aspect-square w-28 p-5">
+         <button className="rounded-full w-full h-full grid place-content-center bg-white">
+            <span className="material-icons !text-4xl !text-black">east</span>
+         </button>
+         <div className="absolute w-full h-full text-white top-0 left-0 text-xs uppercase -z-10 animate-[spin_15s_linear_infinite]">
+            {stringArray.map((char, i) => (
+               <span
+                  key={i}
+                  style={{
+                     rotate: `${i * 12}deg`,
+                  }}
+                  className="absolute left-1/2 h-1/2 origin-bottom-left "
+               >
+                  {char}
+               </span>
+            ))}
+         </div>
+      </div>
+   );
+}
--- src/components/Home/Slider/SliderButtons.tsx
+++ src/components/Home/Slider/SliderButtons.tsx
@@ -1,6 +1,7 @@
 import NavigationButton from "./NavigationButton";
 import { useSwiper } from "swiper/react";
 import PaginationButton from "./PaginationButton";
+import ExploreMore from "./ExploreMore";
 
 type Props = { activeIndex: number };
 
@@ -10,15 +11,18 @@ export default function SliderButtons({ activeIndex }: Props) {
    const back = () => slider.slidePrev();
 
    return (
-      <div className="bg-gradient-to-l from-black/60 to-transparent flex flex-col items-center justify-end absolute w-20 top-0 right-0 h-full z-10">
-         <div className="flex flex-col space-y-3 mb-5">
-            <PaginationButton index={0} activeIndex={activeIndex} />
-            <PaginationButton index={1} activeIndex={activeIndex} />
-            <PaginationButton index={2} activeIndex={activeIndex} />
-         </div>
-         <div className="space-y-2 py-5 flex flex-col items-center">
-            <NavigationButton onClick={back} up />
-            <NavigationButton onClick={next} />
+      <div className="bg-gradient-to-l from-black/60 to-transparent flex flex-col items-end justify-between absolute top-0 right-0 h-full z-10 p-5">
+         <ExploreMore />
+         <div className="space-y-10">
+            <div className="flex flex-col items-center space-y-3">
+               <PaginationButton index={0} activeIndex={activeIndex} />
+               <PaginationButton index={1} activeIndex={activeIndex} />
+               <PaginationButton index={2} activeIndex={activeIndex} />
+            </div>
+            <div className="hidden lg:flex flex-col items-center space-y-2">
+               <NavigationButton onClick={back} up />
+               <NavigationButton onClick={next} />
+            </div>
          </div>
       </div>
    );
